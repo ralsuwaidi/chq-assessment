775a32bbb00e1993349dc2777b9d5985cff67c2e
rashed <x.suwaidi@gmail.com>
2022-09-18 21:36:38 +0400

    added courseContents table

--- src/views/app/gogo/start.js
+++ src/views/app/gogo/start.js
@@ -2,28 +2,12 @@ import React from 'react';
 import { Row } from 'reactstrap';
 import { Colxx, Separator } from 'components/common/CustomBootstrap';
 import Breadcrumb from 'containers/navs/Breadcrumb';
-import BestSellers from 'containers/dashboards/BestSellers';
-import { useQuery, gql } from '@apollo/client';
+import CourseContents from 'containers/pluralsight/CourseContents';
+import ImageCardList from 'containers/ui/ImageCardList';
 
-const COURSE_CATALOG = gql`
-  query {
-    courseCatalog(first: 10) {
-      nodes {
-        title
-      }
-    }
-  }
-`;
 
 const Start = ({ match }) => {
-  const { loading, error, data } = useQuery(COURSE_CATALOG);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  console.log(error);
-  if (!loading) {
-    console.log(data);
-  }
 
   return (
     <>
@@ -35,8 +19,11 @@ const Start = ({ match }) => {
       </Row>
       <Row>
         <Colxx lg="12" xl="12" className="mb-4">
-          <BestSellers title="dashboards.top-viewed-posts" />
+          <CourseContents title="dashboards.top-viewed-posts" />
         </Colxx>
+      </Row>
+      <Row>
+        <ImageCardList/>
       </Row>
     </>
   );
