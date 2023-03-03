619a6997a62b5ce5d26f2453125fe5c300c2fa85
rashed <x.suwaidi@gmail.com>
2023-03-03 14:11:59 +0400

    change spinner size

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,10 +2,11 @@ import { Spinner } from "flowbite-react";
 import React from "react";
 
 const Loading = () => (
-  <div className="loading">
-    <Spinner aria-label="Default status example" />
-
+  <div class="grid h-screen place-items-center">
+    <Spinner size={'xl'} aria-label="Default status example" />
   </div>
+
+
 );
 
 export default Loading;
